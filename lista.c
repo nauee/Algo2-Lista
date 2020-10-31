48b6b6bd9b88b128cb805cba63c3257d627ecfb1
nauee <naheulxd@gmail.com>
2020-10-31 18:44:42 -0300

    Agregadas las pruebas

---
+++ lista.c
@@ -0,0 +1,22 @@
+#include "lista.h"
+
+/*
+
+    typedef struct nodo{
+        void* elemento;
+        struct nodo* siguiente;
+    }nodo_t;
+
+    typedef struct lista{
+        nodo_t* nodo_inicio;
+        nodo_t* nodo_fin;
+        size_t cantidad;
+    }lista_t;
+
+    typedef struct lista_iterador{
+        nodo_t* corriente;
+        lista_t* lista;
+    }lista_iterador_t;
+
+*/
+
